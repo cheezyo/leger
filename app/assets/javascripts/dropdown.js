26165262df0a66521dbbd1461d087330e1782d09
Cezar Sincan <cezarsincan@gmail.com>
2013-02-11 19:07:55 +0100

    df

--- app/assets/javascripts/dropdown.js
+++ app/assets/javascripts/dropdown.js
@@ -1,36 +1,45 @@
-function showstuff(boxid, i, linkid) {
+function showHud(boxid, i, linkid) {
 				var s = document.getElementById(boxid);
 				var link = document.getElementById(linkid);
 				if(s.style.display == "block" && i == 0) {
 					s.style.display = "none";
+					//link.style.background = "#5A668C";
 					link.style.textDecoration = "none";
 					
 				}else if(i == 1) {
 						s.style.display = "none";
 						link.style.textDecoration = "none";
+						
+					//	link.style.background = "#5A668C";
 					}else{
 					s.style.display = "block";
 					link.style.textDecoration = "underline";
+					
+					//link.style.background = "#EA2E49";
 					
 				} 
 
 			}
 			
-function showstuff2(boxid, i, linkid) {
+function showKirurgi(boxid, i, linkid) {
 				var s = document.getElementById(boxid);
 				var link = document.getElementById(linkid);
 				if(s.style.display == "block" && i == 0) {
 					s.style.display = "none";
+					//link.style.background = "#5A668C";
 					link.style.textDecoration = "none";
 					
 				}else if(i == 1) {
 						s.style.display = "none";
 						link.style.textDecoration = "none";
+					//	link.style.background = "#5A668C";
 					}else{
 					s.style.display = "block";
 					link.style.textDecoration = "underline";
 					
+					//link.style.background = "#EA2E49";
 					
 				} 
 
 			}
+			
